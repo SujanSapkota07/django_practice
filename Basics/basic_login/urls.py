b84ace8d6ed88c6bd25a97a0739d9e95480242a9
SujanSapkota07 <sujeansapkota47@gmail.com>
2023-11-14 09:48:15 +0545

    first commit

---
+++ Basics/basic_login/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("/login_page.html", views.login_page, name='login_page'),
+    path("", views.landing_page, name ='landing_page'),
+
+
+]
---
+++ Basics/basic_login/views.py
@@ -0,0 +1,18 @@
+from urllib import request
+from django.http import FileResponse, HttpResponse
+from django.shortcuts import redirect, render
+
+
+# Create your views here.
+
+def login_page(request):
+    return render(request, 'login_page.html')
+
+def landing_page(request):
+    return render(request, 'landing_page.html')
+
+def my_image(request):
+    img = open('basic_login/templates/images/sujan.jpeg', 'rb')
+    response = FileResponse(img)
+    return response
+
